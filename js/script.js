32168d40b33faf9bd5458acccaf2d451f02f4ce0
松浦一樹 <matsuurakazuki@matsuurakazukinoMacBook-Air.local>
2023-08-15 15:49:05 +0900

    ダークカラー追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 const loading = document.querySelector('#loading');
 
+// 定数loading（ロード中）という要素にloaded（ロード終了後）の要素を足す
 window.addEventListener('load', () => {
   loading.classList.add('loaded');
 });
+
+const btn = document.querySelector('#btn');
+
+// 一回ではなくボタンがクリックされるたび処理をしたいのでtoggleを使う
+btn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+
+  // もしボタンのテキストが「ダークモードにする」になっているなら
+  if (btn.textContent === 'ダークモードにする'){
+    // クリックされた時に「ライトモードにする」に変更する
+    btn.textContent = 'ライトモードにする';
+
+  //そうでないなら（「ライトモードにする」と表示されているなら） 
+  } else {
+    // クリックされた時に「ダークモードにする」に戻す
+    btn.textContent = 'ダークモードにする';
+  }
+
+})
