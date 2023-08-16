dc741e5789ff9c44d251c392efa35984e37f8503
松浦一樹 <matsuurakazuki@matsuurakazukinoMacBook-Air.local>
2023-08-16 15:54:28 +0900

    プログレスバーの追加

--- js/script.js
+++ js/script.js
@@ -57,3 +57,27 @@ isAgreed.addEventListener('change', () => {
   //チェックボックスとdisabledが逆になっているので!を文頭につけてチェックされていないとdisabledをtrueにする文を書く
   checkBtn.disabled = !isAgreed.checked;
 })
+
+  //スクロール量の表示をする実装
+  // 処理が長くなりそうなので関数名を指定
+  const getScrollPercent = () => {
+    // スクロール量
+    const scrolled = window.scrollY;
+    // 縦方向のスクロールはY、横方向はXで指定する
+    // console.log(`${scrolled}スクロールされました`);
+    
+    // ページ全体の高さ
+    const pageHeight = document.documentElement.scrollHeight;
+
+    // 表示領域の高さ
+    const viewHeight = document.documentElement.clientHeight;
+    // console.log(`ページの高さ:${pageHeight}、表示領域の高さ:${viewHeight}`)
+    
+    // 0~100でスクロールされた割合を出す計算式 スクロール量 / スクロール可能なページの高さ（ページ全体の高さ - 表示領域の高さ）*100
+    const percentage = scrolled / (pageHeight - viewHeight) * 100;
+    // console.log(percentage);
+
+    // プログレスバーに幅を追加
+    document.querySelector('#bar').style.width = `${percentage}%`;
+  }
+window.addEventListener('scroll', getScrollPercent);
