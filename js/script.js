8673ae79c9fdb9f56611a824dc10e334324fed3b
松浦一樹 <matsuurakazuki@matsuurakazukinoMacBook-Air.local>
2023-08-16 09:24:27 +0900

    文字数カウンター追加

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ローディングの実装
 const loading = document.querySelector('#loading');
 
 // 定数loading（ロード中）という要素にloaded（ロード終了後）の要素を足す
@@ -5,6 +6,7 @@ window.addEventListener('load', () => {
   loading.classList.add('loaded');
 });
 
+// ダークモードの実装
 const btn = document.querySelector('#btn');
 
 // 一回ではなくボタンがクリックされるたび処理をしたいのでtoggleを使う
@@ -21,5 +23,21 @@ btn.addEventListener('click', () => {
     // クリックされた時に「ダークモードにする」に戻す
     btn.textContent = 'ダークモードにする';
   }
-
 })
+
+// 文字数カウンターの実装
+const text = document.querySelector('#text');
+const count = document.querySelector('#count');
+
+// テキストを入力した時に文字数を数える
+text.addEventListener('keyup', () => {
+  count.textContent = text.value.length;
+
+  // もし100文字を超えた時
+  if (text.value.length > 100) {
+    count.classList.add('alert');
+    // そうでないなら（100文字以下なら）
+  } else {
+    count.classList.remove('alert');
+  }
+});
